5706508a79fc990c4088b42426e9933011f4ae4f
SMaire <sullyvan.maire@hotmail.com>
2022-06-13 10:20:44 +0200

    UP : MongoDB Docker basic config

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
   }));
+
+  await prisma.$connect();
+
+  //const users = await prisma.user.findMany();
+
   await app.listen(3333);
 }
 bootstrap();
+
