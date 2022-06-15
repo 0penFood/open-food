3c4fb41d1f0111d0b2662f982095ccc95649511a
vfastrez <vincent.fastrez@gmail.com>
2022-06-15 10:07:35 +0200

    ADD: Logger Helper Setup + Cleaning trashy folders

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { RestaurantsModule } from './restaurants/restaurants.module';
-import { DriversModule } from './drivers/drivers.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { PaymentsModule } from './payments/payments.module';
-import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, RestaurantsModule, DriversModule, PrismaModule, PaymentsModule, OrdersModule],
+  imports: [PrismaModule],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger} from '../helpers/logger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
   }));
+  await Logger.infoLog('server', 'Server started');
   await app.listen(3333);
 }
 bootstrap();
