13a940a611e25c96d36ae2b560c05dbba6e84a4b
vfastrez <vincent.fastrez@gmail.com>
2022-06-20 11:06:36 +0200

    ADD: SwaggerModule and swagger generation

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,19 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger} from '../helpers/logger';
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+      .setTitle('Open Food API')
+      .setDescription('An API for Open Food')
+      .setVersion('1.0')
+      .addTag('openfood')
+      .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
   }));
